fa43681fefd5414351bc9184ca65167f7f7959e2
Tom Chen <tom@itri.org.tw>
2019-11-24 17:17:21 +0800

    Add shorthand

--- 02-var/main.go
+++ 02-var/main.go
@@ -1,12 +1,33 @@
 package main
 
-import "fmt"
-import "syreclabs.com/go/faker"
+import (
+	"fmt"
+
+	"syreclabs.com/go/faker"
+)
 
 func main() {
 
-	var fakerNameTest = faker.Name().String
 	// Using var
-	// var name = "TOM"
+	var fakerNameTest = faker.Name().String
+	// var name = "TOM: " + faker.RandomString(10)
+
+	var ageTest int64 = 100
+	const isCool = true
+	// isCool = false
+
+	// Shorthand
+	// name := "tom"
+	// email := "tom@test.com"
+	size := 1.238
+	// var size float32 = 1.238
+
+	name, email := "jack", "jack@test.com"
+
 	fmt.Println("Hello, ", fakerNameTest())
+
+	fmt.Println(name, ageTest, isCool, size, email)
+
+	fmt.Printf("%T\n", fakerNameTest)
+	fmt.Printf("%T\n", size)
 }
