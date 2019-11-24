0826e9815526847b66f95ed87aed7cce760af194
Tom Chen <tom@itri.org.tw>
2019-11-24 22:38:26 +0800

    Add test multiple values

--- 01-hello/main.go
+++ 01-hello/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+import "syreclabs.com/go/faker"
+
 func main() {
 	fmt.Println("hello, tom go app2 !!")
+
+	username, time := test()
+	fmt.Println("test multiple values return")
+	fmt.Println(username, time)
+
+}
+
+// multiple value
+func test() (string, int) {
+	fName := faker.Name().FirstName
+	fNumber := faker.RandomInt(10,100)
+	
+	return fName(), fNumber
 }
---
+++ 08-maps/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//todo: https://youtu.be/SqrbIlUwR0U
+	fmt.Println("hello, maps, mean key value")
+}
