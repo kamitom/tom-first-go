869b8bc818413ef1d2b15dab5ac37f5a494fcf73
kamitom csw <csw@pm.me>
2020-09-26 21:52:23 +0800

    chore: update main.go

--- 02-var/main.go
+++ 02-var/main.go
@@ -9,25 +9,35 @@ import (
 func main() {
 
 	// Using var
-	var fakerNameTest = faker.Name()
+	var fakerNameTest = faker.Name().FirstName()
+	var fullName = faker.Name()
 	// var name = "TOM: " + faker.RandomString(10)
 
-	var ageTest int64 = 100
+	// var ageTest int64 = 100
 	const isCool = true
 	// isCool = false
 
 	// Shorthand
-	// name := "tom"
+	nameShorthand := "tom"
 	// email := "tom@test.com"
 	size := 1.238
 	// var size float32 = 1.238
 
-	name, email := "jack", "jack@test.com"
+	name, email, newfakername := fullName, fakerNameTest+"@test.com", fakerNameTest
 
-	fmt.Println("Hello2, ", fakerNameTest)
+	fmt.Println("Hello shorthand name, ", nameShorthand)
 
-	fmt.Println(name, ageTest, isCool, size, email)
+	// fmt.Println(name, ageTest, isCool, size, email)
 
-	fmt.Printf("%T\n", fakerNameTest)
+	fmt.Printf("%T\n", isCool)
 	fmt.Printf("%T\n", size)
+
+	fmt.Println(name, email, newfakername)
+
+	fmt.Println(HelloWorld(fakerNameTest))
+
+}
+
+func HelloWorld(user_name string) string {
+	return fmt.Sprintf("Hi, %s ", user_name)
 }
